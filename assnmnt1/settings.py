e046b16d0c3fb6409b649574527a550530435e18
SanjanaPoptani <sanjanapoptani54@gmail.com>
2021-08-16 23:21:50 +0530

    changed settings

--- assnmnt1/settings.py
+++ assnmnt1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l!((s#zd9w9yxri*ctj_4wame^!4@80)88aea7h3o0w0&znsn_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
